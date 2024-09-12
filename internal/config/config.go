5fca8e696a4c312256d95fba0ddcd3e7b77e75f7
Vladimir <vleypi@gmail.com>
2024-09-12 23:53:41 +0300

    first commit

---
+++ internal/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Adress      string        `yaml:"adress" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() {
+
+}
